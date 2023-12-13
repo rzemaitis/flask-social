a58abfc979e6641c262446ef386e2aa3a66a66c8
Rokas Žemaitis <rokas.zemaitis2@gmail.com>
2023-12-13 21:56:56 +0000

    Add auth html templates, fix auth.py bug
    Added html templates to templates/auth/.
    
    Fixed a duplicated login view bug in auth.py.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -63,31 +63,6 @@ def login():
 
     return render_template('auth/login.html')
 
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-
-        flash(error)
-
-    return render_template('auth/login.html')
-
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
