1c282746394440c71602aa6dbfb4525db102d6d5
Rokas Žemaitis <rokas.zemaitis2@gmail.com>
2023-12-16 23:17:21 +0000

    Add blog functionality
    Added blog.py and its constituent views in html.

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -32,5 +32,10 @@ def create_app(test_config=None):
     db.init_app(app)
 
     from . import auth
+    from . import blog
+
     app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
+
     return app
